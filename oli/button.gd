535c0839d46ffb1a1d983c4baf153345dfef1266
Arpa-exe <oli.laroch@gmail.com>
2025-03-22 13:26:11 -0400

    button fades to black

--- oli/button.gd
+++ oli/button.gd
@@ -1,12 +1,19 @@
 extends AnimatedSprite2D
 
 @onready var button: AnimatedSprite2D = $"."
+@onready var color_rect: ColorRect = $ColorRect
 
 var can_press = false
+var pressed = false
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and can_press:
 		button.play("pressed")
+		pressed = true
+	if pressed:
+		color_rect.size = get_viewport_rect().size
+		color_rect.position = Vector2.ZERO - Vector2(color_rect.size.x/2, color_rect.size.y/2)
+		color_rect.color = color_rect.color.lerp(Color.BLACK, 0.1)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	can_press = true
