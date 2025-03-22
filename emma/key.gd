2ddf1ea8e32908466ad88b26f6bbc32711c3f7ae
le1946970 <Emma.Trottier@stu.crc-lennox.qc.ca>
2025-03-22 13:09:37 -0400

    adding collectibles

---
+++ emma/key.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+const FOLLOW_SPEED = 4.0
+
+var player
+var following = false
+var offset = 10
+
+func _process(delta):
+	if following and player:
+		var anim = player.get_node("AnimatedSprite2D")
+		if anim != null:
+			if anim.flip_h:
+				position = position.lerp(player.position + Vector2(offset, 0), delta * FOLLOW_SPEED)
+			else:
+				position = position.lerp(player.position - Vector2(offset, 0), delta * FOLLOW_SPEED)
+		else:
+			position = position.lerp(player.position, delta * FOLLOW_SPEED)
+		
+
+func _on_body_entered(body: Node2D) -> void:
+	player = body
+	following = true
