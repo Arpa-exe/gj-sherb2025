e07578c6e09e6977771ef19081a12b9f29260a18
Arpa-exe <oli.laroch@gmail.com>
2025-03-23 00:21:32 -0400

    aaaaaaaaaaaa i lied

--- emma/global.gd
+++ emma/global.gd
@@ -9,7 +9,7 @@ var currentHealth
 var level1Scene = "res://jade-murtaza/Level1/World/Level1.tscn"
 var level2Scene = "res://jade-murtaza/Level3/world/Level3.tscn"
 var level3Scene = "res://jade-murtaza/Level2/world/Level2Water.tscn"
-var endScene = "res://emma/gameTest.tscn"
+var endScene = "res://emma/you_won.tscn"
 
 var controlScene = "res://emma/controls.tscn"
 var mainMenuScene = "res://emma/main_menu.tscn"
--- oli/button.gd
+++ oli/button.gd
@@ -21,11 +21,13 @@ func _process(delta: float) -> void:
 		color_rect.position = Vector2.ZERO - Vector2(color_rect.size.x/2, color_rect.size.y/2)
 		color_rect.color = color_rect.color.lerp(Color.BLACK, 0.01)
 		timer += 1
-		if timer > 50:
+		if timer > 120:
 			if Global.currentLevel == "1": # changed level 1 to 2 
 				get_tree().change_scene_to_file(Global.level2Scene) # changed level 2 to 1
 			elif Global.currentLevel == "2": # changed level 2 to 1
 				get_tree().change_scene_to_file(Global.level3Scene)
+			elif Global.currentLevel == "3": # changed level 2 to 1
+				get_tree().change_scene_to_file(Global.endScene)
 
 func _on_body_entered(body: Node2D) -> void:
 	can_press = true
